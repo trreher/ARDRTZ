d1b3c16a8ef97f437249b4ac82263a5a9de3c8bf
Tyler Reher <trreher@gmail.com>
2016-04-02 13:45:10 -0500

    adding Invoice collection code

--- src/finalProjectDRTZ/InvoiceCollection.java
+++ src/finalProjectDRTZ/InvoiceCollection.java
@@ -1,5 +1,52 @@
 package finalProjectDRTZ;
+import java.util.ArrayList;
 
 public class InvoiceCollection {
+	
+	private ArrayList<Invoice> invoices;
 
+	public Invoices()
+	{
+		invoices = new ArrayList<Invoice>();
+	}
+	
+	public Invoice find(int _id)
+	{
+		return null;
+	}
+
+	public void addInvoice(Invoice i)
+	{
+		invoices.add(i);
+	}
+	
+	public void removeInvoice(Invoice i)
+	{
+		invoices.remove(i);
+	}
+	
+	public double customerInvoiceTotal(int _custId, String begin, String end)
+	{
+		return 0;
+	}
+
+	public double customerOutstandingInvoiceBalance(int _custId, String begin, String end)
+	{
+		return 0;
+	}
+	
+	public int customerInvoiceCount(int _custId, String begin, String end)
+	{
+		return 0;
+	}
+	
+	public int customerOpenInvoiceCount(int _custId, String begin, String end)
+	{
+		return 0;
+	}
+	
+	public int customerClosedInvoiceCount(int _custId, String begin, String end)
+	{
+		return 0;
+	}
 }
